9090f233e27ee065506690a7755d38d2ab34337f
Adejare <adejare.arasheed@gmail.com>
2022-09-22 01:33:57 +0100

    if, else and loop functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(9):
     for k in range(i, 10):
-        if ((i != k) and (i != 8 or k != 9)):           
+        if ((i != k) and (i != 8 or k != 9)):
             print("{}{}".format(i, k), end=", ")
 print(f"{i}{k}")
