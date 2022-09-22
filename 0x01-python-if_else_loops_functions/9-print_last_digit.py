a3300155c01c44cf14ed37899f23a5e3997346bf
Adejare <adejare.arasheed@gmail.com>
2022-09-22 01:00:13 +0100

    if, else and loop functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-	return number % 10
+	if number >= 0:
+		digit = number % 10
+	else:
+		digit = -number % 10
+	print("{}".format(digit), end="")
+	return digit
