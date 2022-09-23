5a252bd11a8895e15ef20ec2c654ed32ba6feb8f
Adejare <adejare.arasheed@gmail.com>
2022-09-23 19:09:17 +0100

    python import

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as func
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        sys.exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        list_ = [('+', func.add(a, b)), ('-', func.sub(a, b)),
+        ('/', func.div(a, b)), ('*', func.mul(a, b))]
+        operators = list('+-/*')
+        if sys.argv[2] not in operators:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+        else:
+            for i, v in list_:
+                if sys.argv[2] == i:
+                    print("{} {} {} = {}".format(a, i, b, v)) 
