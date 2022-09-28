64c706ae9aa8c4cc51ee14537a82c82cfcd93294
Adejare <adejare.arasheed@gmail.com>
2022-09-28 22:18:57 +0100

    More Data Structures: Set and Dictionary

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[[]]):
-    return [list(map(lambda x: x ** 2, l)) for l in matrix if l != [[]]]
+def square_matrix_map(matrix=[]):
+    return [list(map(lambda x: x ** 2, l_)) for l_ in matrix if l_ != [[]]]
