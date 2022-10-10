e460dc8fc656eae29168ad7c195ff4e115a24f07
Adejare <adejare.arasheed@gmail.com>
2022-10-10 20:42:24 +0100

    Python -Exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,14 +11,11 @@ def safe_print_list_integers(my_list=[], x=0):
             real number
     """
     count = 0
-    if my_list != []:
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-            except IndexError as idx:
-                print("IndexError:", idx)
-            except Exception:
-                pass
-        print('')
-        return count
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except Exception:
+            pass
+    print('')
+    return count
