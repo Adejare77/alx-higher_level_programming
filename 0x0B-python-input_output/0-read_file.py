3999b94fa79a293aa9571746c0a3928fd6c3cc16
Adejare <adejare.arasheed@gmail.com>
2022-11-01 20:38:26 +0100

    Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-""" Handling Files """
+""" Read File """
 import sys
 
 
 def read_file(filename=""):
+    """ reads a text file (uft8) and prints it to stdout
+
+        Args:
+            filename: name of the file to be read
+    """
     with open(filename, 'r', encoding='utf-8') as fr:
         sys.stdout.write(fr.read())
--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -18,8 +18,8 @@ class Student:
 
     def to_json(self, attrs=None):
         """ retrieves a dictionary representation of a student """
-        #if (type(attrs) == list and all(type(u) == str for u in attrs)):
-        if (isinstance(attrs, list) and all(isinstance(u, str) for u in attrs)):
+        if (isinstance(attrs, list) and all(isinstance(u, str)
+                                            for u in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
