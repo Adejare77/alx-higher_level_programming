6255b6e908006a270c46f13194d9a58d7b08e2c5
Adejare <adejare.arasheed@gmail.com>
2024-01-18 14:29:18 +0100

    More SQL queries

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,28 @@
+-- Active: 1705509733487@@127.0.0.1@3306@hbtn_0d_tvshows
+-- uses the "hbtn_0d_tvshows" DATABASE to list all genres `not linked to the show` `Dexter`
+-- tv_shows table contains only one record where title = Dexter (but id may differ)
+-- Results must be sorted in ascending order by the genre name
+-- You can use a maximum of two SELECT statement
+-- The DATABASE name will be passed as an argument of the mysql command
+--
+-- WITH my_table AS (
+--     SELECT id, genre_id
+--     FROM tv_shows
+--     JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+--     WHERE title = 'Dexter'
+-- )
+-- SELECT id, genre_id
+-- FROM my_table;
+
+WITH my_table AS (
+    SELECT id, genre_id as my_genre_id
+    FROM tv_shows
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    WHERE title = 'Dexter'
+)
+SELECT DISTINCT name
+FROM tv_show_genres
+LEFT JOIN my_table ON my_table.my_genre_id = tv_show_genres.genre_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE my_genre_id IS NULL
+ORDER BY name
