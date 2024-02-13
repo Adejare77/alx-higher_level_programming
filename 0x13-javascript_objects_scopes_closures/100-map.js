e4ad12388899111f897eeaf9adfe884b6ec286ed
Adejare <adejare.arasheed@gmail.com>
2024-02-13 19:36:20 +0100

    JavaScript

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const list = require('./100-data');
+const list = require('./100-data').list;
 console.log(list);
 
-// const custom_list = list.map((x) => x * (x - 1));
-// console.log(custom_list);
+const custom_list = list.map((x) => x * (x - 1));
+console.log(custom_list);
