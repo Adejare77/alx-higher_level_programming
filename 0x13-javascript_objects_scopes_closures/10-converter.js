e72b0d4306cbe73edd996187d98321d588a9c00b
Adejare <adejare.arasheed@gmail.com>
2024-02-14 21:08:51 +0100

    JavaScript

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  console.log(`The base passed is: ${base}`);
   return function (num) {
-    console.log(`The num passed is: ${num}`);
-    console.log(`The num.to.string(base) is: ${num.toString(base)}`);
     return num.toString(base);
   };
 };
