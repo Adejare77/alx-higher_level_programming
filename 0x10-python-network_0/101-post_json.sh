4559b1547d34007df524c21728fab4c08ead0c17
Adejare <adejare.arasheed@gmail.com>
2024-02-29 18:49:14 +0100

    Python Networking

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Sends a JSON POST request to a URL passed as first argument, and displays the
-# body of the response
-# Where the second argument is a JSON File
+# Sends a JSON POST request to a URL passed as first argument, and displays the Where the second argument is a JSON File
 curl -sH "Content-Type: application/json" -X POST -d "$(cat "$2")" "$1"
