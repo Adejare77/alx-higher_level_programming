fa9fc9e11442ddebee4001c7fa091b0a3d526aae
Adejare <adejare.arasheed@gmail.com>
2024-02-29 18:46:16 +0100

    Python Networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Takes in a URL, sends a request to that URL, and displays the size
 # of the body of the response
 
-curl -sI "$1" | grep -i "content-length"
+curl -sI "$1" | grep -i "content-length" | cut -d " " -f 2
