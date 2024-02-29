1fc420a4fdcdb0ae509c7837ee74c42cb5f847ad
Adejare <adejare.arasheed@gmail.com>
2024-02-29 19:25:17 +0100

    Python Networking

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI "$1" | gerep -i "http" | cut -d " " -zf 2
+curl -sI "$1" | grep -i "http" | cut -d " " -zf 2
