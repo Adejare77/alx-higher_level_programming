74db980977abc654365cb9afd0252705f7b60fd2
Adejare <adejare.arasheed@gmail.com>
2024-03-05 21:33:54 +0100

    Python Network

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""takes your GitHub credentials (username and password) and uses
+the GitHub API to display your id
+"""
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    resp = requests.get(url, auth=(argv[1], argv[2]))
+
+    if resp.status_code == 200:
+        content = resp.content
+        json_data = resp.json()
+        print(json_data['id'])
+    else:
+        print("None")
